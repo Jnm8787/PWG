7d82b8da62033300a5624b4d918aa79f1ce97d36
JNM87 <J.molina22587@gmail.com>
2021-10-10 22:13:53 -0400

    fixed password generator

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# PWG
+# Password Generator
+Generates a secure password using HTML, CSS, and Javascript.Parameters include length,lowercase letters, uppercase letters, numbers and special characters. 
+
+[View Link](https://jnm8787.github.io/05-password-homework/)
+
+## Screenshots
+![image](./images/pwg.png)
---
+++ script.js
@@ -0,0 +1,67 @@
+
+const generate = document.querySelector('#generate');
+
+const lengthCheckbox = document.querySelector('#length');
+const lowercaseCheckbox = document.querySelector('#lowerBox');
+const uppercaseCheckbox = document.querySelector('#upperBox');
+const numberCheckbox = document.querySelector('#numbersBox');
+const specialCharCheckbox = document.querySelector('#specialCharBox');
+
+//password to text field
+function writePass() {
+	let password = generatePass();
+	let passwordText = document.querySelector('#password');
+	passwordText.value = password;
+}
+
+function generatePass() {
+
+	//password paramaters 
+	const lower = 'abcdefghijklmnopqrstuvwxyz'.split('');
+	const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+	const numbers = '0123456789'.split('');
+	const specialChar = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~".split('');
+
+	
+	let passChoices = [];
+
+	let passBuilder = [];
+
+	let passLength = parseInt(lengthCheckbox.value);
+
+	// add selected character types to passChoices
+	if (lowercaseCheckbox.checked) {
+		passChoices = passChoices.concat(lower);
+	}
+	if (uppercaseCheckbox.checked) {
+		passChoices = passChoices.concat(upper);
+	}
+	if (numberCheckbox.checked) {
+		passChoices = passChoices.concat(numbers);
+	}
+	if (specialCharCheckbox.checked) {
+		passChoices = passChoices.concat(specialChar);
+	}
+
+
+	// Validate length
+	if (passChoices.length) {
+		if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
+			alert('Please enter a password length between 8 and 128 characters.');
+			return '';
+		} else {
+			
+			for (let i = 0; i < passLength; i++) {
+				let randChar = Math.floor(Math.random() * passChoices.length);
+				passBuilder.push(passChoices[randChar]);
+			}
+			let password = passBuilder.join('');
+			return password;
+		}
+	} else {
+		alert('Please select character types.');
+		return '';
+	}
+}
+
+generate.addEventListener('click', writePass);
